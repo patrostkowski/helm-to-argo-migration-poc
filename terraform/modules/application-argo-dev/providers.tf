a1aea3b8cdd6d688c21038c36a07f95451141eeb
Patryk Rostkowski <patrostkowski@gmail.com>
2023-10-28 14:40:33 +0200

    adjust

--- terraform/modules/application-argo-dev/providers.tf
+++ terraform/modules/application-argo-dev/providers.tf
@@ -8,3 +8,9 @@ provider "argocd" {
   server_addr = "localhost:8080"
   auth_token  = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcmdvY2QiLCJzdWIiOiJ1c2VyOmFwaUtleSIsIm5iZiI6MTY5ODQ0MTMxMSwiaWF0IjoxNjk4NDQxMzExLCJqdGkiOiJ1c2VyIn0.L3y7zM05llL1WNQ3zj4mMeTGNJbizIAzFvkrybSIUlc"
 }
+
+provider "gitops" {
+  repo_url = "https://github.com/patrostkowski/helm-to-argo-migration-poc.git"
+  branch   = "main"
+  path     = ".gitops"
+}
--- terraform/modules/application-argo-dev/terraform.tf
+++ terraform/modules/application-argo-dev/terraform.tf
@@ -4,5 +4,9 @@ terraform {
       source  = "oboukili/argocd"
       version = "6.0.3"
     }
+    gitops = {
+      source  = "tyler-technologies/gitops"
+      version = "0.0.3-rc"
+    }
   }
 }
